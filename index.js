c7212d44f8932cfa89f3513e4472b33d53add92a
Murilo Krugner <murilokrugner@hotmail.com>
2019-05-11 19:27:17 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import App from './App';
+import {name as appName} from './app.json';
+
+//var NativeAppEventEmitter = require('RCTNativeAppEventEmitter')
+
+AppRegistry.registerComponent(appName, () => App);
